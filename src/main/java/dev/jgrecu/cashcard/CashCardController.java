f6e77008106d7ff3e0d99ab43e287685647be803
Jeremy Grecu <jgrecu@gmail.com>
2023-04-24 20:33:28 +0100

    Implement GET all, and Paging and Sorting

--- src/main/java/dev/jgrecu/cashcard/CashCardController.java
+++ src/main/java/dev/jgrecu/cashcard/CashCardController.java
@@ -1,5 +1,9 @@
 package dev.jgrecu.cashcard;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -32,5 +36,16 @@ public class CashCardController {
                 .toUri();
 
         return ResponseEntity.created(locationOfNewCashCard).build();
+    }
+
+    @GetMapping()
+    public ResponseEntity<Iterable<CashCard>> findAll(Pageable pageable) {
+        Page<CashCard> page = cashCardRepository.findAll(
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize(),
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+                ));
+        return ResponseEntity.ok(page.toList());
     }
 }
